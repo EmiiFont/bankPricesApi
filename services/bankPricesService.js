dd9300e35ad2678c3c935ae93c3f7ef9a5328c27
Emilio Font <emifont01@gmail.com>
2019-10-18 11:39:34 -0400

    added missing file from last commit

--- services/bankPricesService.js
+++ services/bankPricesService.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const admin = require('firebase-admin');
+const serviceAccount = require('../bankpricesstore-firebase-cred.json');
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+const addPrice = (bankPrice) => {
+    db.collection(bankPrice.name).doc(bankPrice.date).set(bankPrice).then((vl)=>{
+      console.log(vl);
+    });
+}
+
+const addBankPrices = (bankPricesArr) => {
+    for(let bank of bankPricesArr){
+        db.collection(bank.name).add(JSON.parse(JSON.stringify(bank))).then((writeResult)=>{
+            console.log(writeResult);
+          });
+    }
+}
+
+module.exports.addBankPrices = addBankPrices;
+module.exports.addPrice = addPrice;
