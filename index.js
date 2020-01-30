f2a752b5f18616c1d68634d9b95a0475b4ee23c4
Emilio Font <emifont01@gmail.com>
2020-01-30 15:10:08 -0500

    added service reference to index

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const app = express()
 const cron = require('node-cron')
 const port = process.env.PORT || 3000;
 const scraper = require('./utils/scraper');
-//const bankService = require('../services/bankPricesService');
+const bankService = require('../services/bankPricesService');
 
 
 const routes = require('./routes/pricesRoutes');
 routes.routes(app);
-
 
 cron.schedule("0 0 */5 * * * *", function() {
   scraper.initNavigation()
