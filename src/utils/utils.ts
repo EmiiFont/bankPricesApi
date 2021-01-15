d7905be580739044a09503d59c788d4b173d35a0
EmiFont <emifont01@gmail.com>
2021-01-15 12:15:57 -0500

    fix: converted parseDecimalFromArrayOfString to typescript and added test

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,3 +11,29 @@ export const getValueForPrices = async (page: puppeteer.Page,
     const price = await page.evaluate(element => element.value, priceElement);
     return parseFloat(price || 0);
 }
+
+export function parseDecimalFromArrayOfString(stringArr: Array<string>): Array<number> {
+    const regexToIgnoreSeparators = /(\d|,)+/g; //sometimes prices came as e.g: 12 33, 12,33 and 12.33
+    const pricesArr: Array<number> = [];
+    const regexWithoutTextContinuing =/\d+[\.\,]\d{0,2}$/;
+    const rregexWitTextContinuing =/\d+[\.\,]\d{0,2}/;
+
+    for (let index = 0; index < stringArr.length; index++) {
+        const priceEmpty =  stringArr[index].match(regexWithoutTextContinuing) || stringArr[index].match(rregexWitTextContinuing);
+        const price = priceEmpty !== null ? priceEmpty.join('') : "";
+        const priceNoComma = price.replace(",",".");
+        const isValidNumber = priceNoComma.match(regexWithoutTextContinuing)?.pop();
+
+        if(parseFloat(isValidNumber || "0") > 0){
+            let parsedNumbersArr =  isValidNumber?.match(regexToIgnoreSeparators);
+            if(parsedNumbersArr != null){
+                if(parsedNumbersArr[0].length == 3){
+                    parsedNumbersArr[0] = parsedNumbersArr[0].substr(1);
+                }
+                const parseNumber = parsedNumbersArr.join(".");
+                pricesArr.push(parseFloat(parseNumber));
+            }
+        }
+    }
+    return pricesArr;
+}
--- tests/util.test.ts
+++ tests/util.test.ts
@@ -1,4 +1,4 @@
-import {getTextContentForPrices, getValueForPrices} from "../src/utils/utils";
+import {getTextContentForPrices, getValueForPrices, parseDecimalFromArrayOfString} from "../src/utils/utils";
 
 describe("utils",() =>{
 
@@ -35,4 +35,58 @@ describe("utils",() =>{
         expect(result).toEqual(50.60);
     });
 
+    it("parseDecimalFromArrayOfString should return array of numbers with extra text", async () =>{
+
+        const text = "Dolares\\nCompra 57.85\\n#Venta 58.30 Siempre hay.\\n#Euro Compra 69.20\\nVenta: 70.60 " +
+            "siempre hay\\n#Suizos a 62.70\\n#Canadas 143.20\\n#Libras Esterlinas 76.70 visítanos " +
+            "y\\nconseguirás precios especiales.\\n";
+        const strArr = text.split("\\n");
+
+        const expected = [57.85,58.30,69.20,70.60,62.70,43.20,76.70];
+        const result = parseDecimalFromArrayOfString(strArr);
+
+        expect(result).toEqual(expected)
+    });
+
+    it("parseDecimalFromArrayOfString should return array of number without extra text", async () =>{
+
+        const text = "Divisas: Compra US$: RD$58.00 - Venta US$: 58.30" +
+            "            Tasas Pasivas: Certificados de Depósito (Desde 5.05% anual)" +
+            "            Tasas Activas: Préstamo para Vehículo (Desde 15.95% Anual)"
+        ;
+        const strArr = text.split("  ")[0].split(" ");
+
+        const expected = [58.0, 58.3];
+        const result = parseDecimalFromArrayOfString(strArr);
+
+        expect(result).toEqual(expected)
+    });
+
+    it("parseDecimalFromArrayOfString should return an empty array", async () =>{
+
+        const result = parseDecimalFromArrayOfString([]);
+
+        expect(result).toEqual([])
+    });
+
+    it("parseDecimalFromArrayOfString should return an empty array if no matching decimal", async () =>{
+
+        const result = parseDecimalFromArrayOfString(["Dolares: 12345 compralos a este precio"]);
+
+        expect(result).toEqual([])
+    });
+
+    it("parseDecimalFromArrayOfString should return array of number with decimal formatted with comma", async () =>{
+
+        const text = "Divisas: Compra US$: RD$58,20 - Venta US$: 58,30" +
+            "            Tasas Pasivas: Certificados de Depósito (Desde 5.05% anual)" +
+            "            Tasas Activas: Préstamo para Vehículo (Desde 15.95% Anual)"
+        ;
+        const strArr = text.split("  ")[0].split(" ");
+
+        const expected = [58.2, 58.3];
+        const result = parseDecimalFromArrayOfString(strArr);
+
+        expect(result).toEqual(expected)
+    });
 })
